1b5e6969a67f87f8a106ad61b449356be528ad3c
divisha-t09 <89679781+divisha-t09@users.noreply.github.com>
2023-03-09 16:21:42 +0530

    Update script.js

--- DAY9- Courses List/script.js
+++ DAY9- Courses List/script.js
@@ -1,8 +1,8 @@
 const search = () => {
   const searchbox = document.getElementById("search").value.toUpperCase();
-  const items = document.getElementById(".course-list")
+  const items = document.getElementById("course-list")
   const course = document.querySelectorAll(".courses")
-  const cname = document.getElementsByTagName("h2")
+  const cname = items.getElementsByTagName("h2")
 
   for (var i = 0; i < cname.length; i++) {
     let match = course[i].getElementsByTagName("h2")[0];
