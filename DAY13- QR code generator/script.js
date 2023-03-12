19dc2bf228faafa42ccebfdf7befd81707ffcea5
divisha-t09 <89679781+divisha-t09@users.noreply.github.com>
2023-03-12 23:11:08 +0530

    Add files via upload

---
+++ DAY13- QR code generator/script.js
@@ -0,0 +1,17 @@
+// const container = document.querySelector(".container"),
+// qrInput = container.querySelector(".form input"),
+// button = container.querySelector(".form button"),
+// qrImg = container.querySelector(".code img");
+
+// button.addEventListener("click", () => {
+//     let qrValue = qrInput.value;
+//     if(!qrValue) return;
+//     button.innerText = "Generating...";
+
+//     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+//     qrImg.addEventListener("load", () => {
+//         container.classList.add("active");
+//         button.innerText = "Generating QR Code";
+//     })
+// });
+
