afa70082df5c463fc3dcf13235b94f1d5ef31eea
divisha-t09 <89679781+divisha-t09@users.noreply.github.com>
2023-04-02 21:13:56 +0530

    Update script.js

--- DAY24- Color theme and Gradient Generator/script.js
+++ DAY24- Color theme and Gradient Generator/script.js
@@ -1,1 +1,63 @@
-const 
+const container = document.querySelector(".container");
+const refreshB = document.querySelector(".refresh-btn");
+
+const maxPaletteBoxes = 32;
+
+const generatePalette = () => {
+    container.innerHTML = "";
+    for (let i = 0; i < maxPaletteBoxes; i++) {
+        let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+        randomHex = `#${randomHex.padStart(6, "0")}`;
+        
+        const color = document.createElement("li");
+        color.classList.add("color");
+        color.innerHTML = `<div class="react-box" style="background: ${randomHex}"></div>
+                           <span class="hex-value">${randomHex}</span>`;
+        color.addEventListener("click", () => copyColor(color, randomHex));
+        container.appendChild(color);
+    }
+}
+generatePalette();
+
+const copyColor = (elem, hexVal) => {
+    const colorElement = elem.querySelector(".hex-value");
+    navigator.clipboard.writeText(hexVal).then(() => {
+        colorElement.innerText = "Copied";
+        setTimeout(() => colorElement.innerText = hexVal, 1000);
+    }).catch(() => alert("Failed to copy the color code!"));
+
+refreshB.addEventListener("click", generatePalette);
+
+
+
+
+const gradientBox = document.querySelector(".gradient-box");
+const selectMenu = document.querySelector(".select-box select");
+const colorInputs = document.querySelectorAll(".colors input");
+const textarea = document.querySelector("textarea");
+const refreshBtn = document.querySelector(".refresh");
+const copyBtn = document.querySelector(".copy");
+const getRandomColor = () => {
+    const randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+    return `#${randomHex}`;
+}
+const generateGradient = (isRandom) => {
+    if(isRandom) {
+        colorInputs[0].value = getRandomColor();
+        colorInputs[1].value = getRandomColor();
+    }
+    const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
+    gradientBox.style.background = gradient;
+    textarea.value = `background: ${gradient};`;
+}
+const copyCode = () => {
+    navigator.clipboard.writeText(textarea.value);
+    copyBtn.innerText = "Code Copied";
+    setTimeout(() => copyBtn.innerText = "Copy Code", 1600);
+}
+colorInputs.forEach(input => {
+    input.addEventListener("input", () => generateGradient(false));
+});
+selectMenu.addEventListener("change", () => generateGradient(false));
+refreshBtn.addEventListener("click", () => generateGradient(true));
+copyBtn.addEventListener("click", copyCode);
